9b666816f469c26ebdc67e9339b7424b33620e6e
prashanth2808 <postoprashanth28@gmail.com>
2024-04-11 11:15:20 +0530

    UI of flipkart

---
+++ e-com-UI/src/Public/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className='bg-gray-200 h-16'>
+    <Link to ='/Headers' >Home</Link>
+          </div>
+  )
+}
+
+export default Home
